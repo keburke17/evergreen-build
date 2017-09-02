76f75e1ed4f89bd3efa0686fa72d01e54290d6d7
keburke17 <keburke17@gmail.com>
2017-09-01 22:05:11 -0500

    added seed setup and pagination

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+Course.destroy_all
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'uofm_test.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each {|row|
+t = Course.new
+t.name = row['Course Title']
+t.subject = row['Subject']
+t.number = row['Catalog Nbr']
+t.units = row['Units']
+t.save
+puts "#{t.name} saved"
+}
+
+puts "There are now #{Course.count} rows in the course table"
